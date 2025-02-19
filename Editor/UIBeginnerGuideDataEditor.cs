8563218db72896cfe9d764e6e702fa8817e399d6
acced <1415351793@qq.com>
2025-02-19 14:28:23 +0800

    初始文件

---
+++ Editor/UIBeginnerGuideDataEditor.cs
@@ -0,0 +1,11 @@
+    using UnityEditor;
+    using UnityEngine;
+
+    public class GuideMenu
+    {
+        [MenuItem("ZBeginnerGuide/Create Beginner Guide", false, 55)]
+        public static void AddList()
+        {
+            Debug.Log("Create Beginner Guide");
+        }
+    }
